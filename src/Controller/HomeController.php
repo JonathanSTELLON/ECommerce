2961fdf7c84806c55c6fb29cf405c97ff554fc73
JonathanSTELLON <stellon.jonathan@gmail.com>
2021-11-09 16:01:23 +0100

    mise en place home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,10 +14,13 @@ class HomeController extends AbstractController{
     /**
      * @Route("/", name="home_index")
      */
-    public function index():Response{
+    public function index(ProductRepository $productRepository):Response{
+
+        $products = $productRepository->findBy([],['createdAt' => 'DESC']);
+        //dd($products);
 
         return $this->render('home/index.html.twig', [
-            
+            'products' => $products
         ]);
     }
     
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use DateTimeImmutable;
+use App\Entity\Category;
+use App\Entity\Product;
+use Bluemmb\Faker\PicsumPhotosProvider;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture{
+
+    public function load(ObjectManager $manager): void{
+
+        //Création de l'objet Faker
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new PicsumPhotosProvider($faker));
+
+        $categories = [
+            'Vins',
+            'Spiritueux',
+            'Bieres',
+        ];
+
+        $arrayCat = [];
+
+        foreach ($categories as $category){
+            //On crée une nouvelle catégorie en bouclant dans le tableau
+            $cat = new Category();
+            $cat->setName($category);
+            $cat->setCreatedAt(new DateTimeImmutable());
+            array_push($arrayCat, $cat);
+            $manager->persist($cat);
+        }
+
+        for ($i = 0; $i < 20; $i++){
+
+            //On crée 20 nouveaux produits
+            $product = new Product();
+            $product->setName($faker->word(2));
+            $product->setCategory($faker->randomElement($arrayCat));
+            $product->setPrice($faker->numberBetween(10,50));
+            $product->setDescription($faker->text(2500));
+            $product->setThumbnail($faker->imageUrl(300, 200, true));
+            $product->setCreatedAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-3 years', 'now', 'Europe/Paris')));
+            $product->setSlug();
+            $manager->persist($product);
+        }
+    
+        //Le flush éxécute le SQL, on ne le fait qu'une fois
+        $manager->flush();
+    }
+}
