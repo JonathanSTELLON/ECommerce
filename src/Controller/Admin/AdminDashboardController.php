d166b90e9e204b8e75dec76502211e732927d539
JonathanSTELLON <stellon.jonathan@gmail.com>
2021-12-07 16:14:00 +0100

    finalize

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
